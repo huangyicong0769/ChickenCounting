26a180aa97743f25c524e0b21106af563954d1f6
hyc0769 <mcrspsht@outlook.com>
2024-10-22 17:13:20 +0800

    fix GD-HIFM

---
+++ tools/test model.py
@@ -0,0 +1,12 @@
+# from os.path import exists
+
+from ultralytics import YOLO
+
+def main():
+        model_type = "yolov8s_pMCA_C2PSSA_GD"
+        model = YOLO(model_type+".yaml")
+        # model = YOLO("ChickenCounting/yolov8s_MCA_AConv_C2PSA_GD_100e_coco2/weights/best.pt")
+        model.train(data='./data/data.yaml', epochs=2, batch=0.80, cache=True, pretrained=False, save=False)
+        
+if __name__ == "__main__":
+    main()
