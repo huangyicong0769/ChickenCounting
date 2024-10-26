2f5e26f544ae406b113c3c2c742dd482b8a742ae
hyc0769 <mcrspsht@outlook.com>
2024-10-26 18:44:14 +0800

    Intro MSSA

--- tools/test model.py
+++ tools/test model.py
@@ -3,10 +3,10 @@
 from ultralytics import YOLO
 
 def main():
-        model_type = "yolov8s_pMCA_C2PSSA_GD"
+        model_type = "yolov8s_MSSA_C2PSSA_GD"
         model = YOLO(model_type+".yaml")
         # model = YOLO("ChickenCounting/yolov8s_MCA_AConv_C2PSA_GD_100e_coco2/weights/best.pt")
-        model.train(data='./data/data.yaml', epochs=2, batch=0.80, cache=True, pretrained=False, save=False)
+        model.train(data='./data/data.yaml', epochs=25, batch=0.80, cache=True, pretrained=False, save=False)
         
 if __name__ == "__main__":
     main()
