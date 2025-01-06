e86ead4e181ffb7f3297d2f278a811eab33e9519
hyc0769 <mcrspsht@outlook.com>
2025-01-06 11:04:10 +0800

    dataset

--- tools/easytrain.py
+++ tools/easytrain.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 
 meta_params={
     'data': './data/data.yaml',
-    'epochs': 200, 
+    'epochs': 120, 
     'batch': 0.85,
     'cache': True, 
     'project': f'ChickenCounting', 
@@ -13,7 +13,7 @@ meta_params={
     'patience': 0,
     'iou_type': 'CIoU',
     'close_mosaic': 0,
-    'pretrained': False,
+    'pretrained': True,
     
     # Default Hyperparameters
     'optimizer': 'auto',
@@ -352,7 +352,7 @@ def main(model_type, __debug=None):
 if __name__ == "__main__":
     warnings.filterwarnings("ignore", category=UserWarning)
     failed = []
-    for model in ["yolov8s_GD_CAFM_FMF_AConv" for _ in range(3)] + ["yolov8s_GD_CAFM_FMF_C2INXB" for _ in range(3)] + ["yolov8s_GD_CAFM_FMF_AConv_C2INXB" for _ in range(3)] + ["yolov8s_GD_CAFM_FMF_C2PSA" for _ in range(3)]:
+    for model in ["yolov8s_GD_CAFM_FMF_AConv"]:
         try:
             main(model_type=model, __debug=None)
         except Exception as e:
--- tools/pretrain.py
+++ tools/pretrain.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
 
 def main():
-    for model_type in ["yolov8s_GD"]:
-        print("Training "+model_type+":")
-        model = YOLO(model_type+".yaml")
-        model.train(data="coco.yaml", epochs=100, batch=0.85, cache=False, project='ChickenCounting', name=model_type+"_100e_coco", plots=False)
+    for model_type in ["yolov8s_GD_CAFM_FMF_AConv"]:
+        # print("Training "+model_type+":")
+        # model = YOLO(model_type+".yaml").load(r"ChickenCounting\yolov8s_GD_CAFM_FMF_AConv_100e_coco\weights\last.pt")
+        # model.train(data="coco.yaml", epochs=100, batch=-1, cache=False, project='ChickenCounting', name=model_type+"_200e_coco", plots=False)
 
-    # print("Resume training "+model_type+":")
-    # model = YOLO("baseline/yolov8s_CBAM_500e_coco3/weights/last.pt")
-    # model.train(resume=True, cache=False)
+        print("Resume training "+model_type+":")
+        model = YOLO(f"ChickenCounting/{model_type}_200e_coco/weights/last.pt")
+        model.train(resume=True)
         
 if __name__ == "__main__":
     main()
